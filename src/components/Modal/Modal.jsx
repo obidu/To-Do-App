52d441fffb0ad3ff5393da6db870981a4213ec03
obidu <ovidiu8mocanu@yahoo.com>
2022-07-14 00:21:31 +0300

    media rules update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -13,12 +13,14 @@ const Modal = (props) => {
     }
 
     return (
+        <div className="modal-main">
         <div className="modal-container">
             <p>Are you sure you want to delete this task?</p>
             <div className="modal-buttons">
                 <button onClick={props.onClick}>Cancel</button>
                 <button onClick={props.onDelete}>Delete</button>
             </div>
+        </div>
         </div>
 
 
